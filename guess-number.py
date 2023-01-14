037d4643a76b0e4f7d953a6fe3ad160c923dcf05
PetrusAriaa <petrusariacr25@gmail.com>
2023-01-15 02:31:41 +0700

    code implementation

--- guess-number.py
+++ guess-number.py
@@ -1,4 +1,18 @@
 from random import randint
 
-print("=== GUESS THE NUMBER!!! ===")
+print("=== GUESS THE NUMBER BETWEEN 1 TO 99!!! ===")
+my_number = randint(1, 100)
 
+for count in range(10):
+    try:
+        guess = int(input(">>> Insert your guess: "))
+        if guess == my_number:
+            print("You guessed it!")
+            break
+        elif guess < my_number:
+            print("Too low!")
+        elif guess > my_number:
+            print("Too high!")
+    except ValueError:
+        print("Invalid input!")
+        continue
