f9e83174aea1a86618896ab4b3e98626fbb9bb35
PetrusAriaa <petrusariacr25@gmail.com>
2023-01-15 03:09:40 +0700

    some refactoring

--- BST_guess.py
+++ BST_guess.py
@@ -1,16 +1,19 @@
 from random import randint
 from time import sleep
 
-MIN = 0
+MIN = 0 #You can edit min and max values here
 MAX = 10
+
 number = randint(MIN, MAX)
 print("Your number is", number)
 sleep(1)
 
 print("RUN SEARCHING...")
-sleep(3)
+sleep(2)
 a, b = MIN, MAX+1
 counter = 0
+
+#BST goes brrrrrr
 while True:
     center = a + (abs(a-b)//2)
     sleep(0.1)
@@ -23,4 +26,3 @@ while True:
     else:
         b = center
     counter += 1
-
