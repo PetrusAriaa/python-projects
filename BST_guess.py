a51121b6332df25e86e503bebf9ae3233e63167a
PetrusAriaa <petrusariacr25@gmail.com>
2023-01-15 03:06:41 +0700

    implement random integer and BST guess

--- BST_guess.py
+++ BST_guess.py
@@ -1,5 +1,26 @@
 from random import randint
+from time import sleep
 
-number = randint(0,100)
+MIN = 0
+MAX = 10
+number = randint(MIN, MAX)
 print("Your number is", number)
+sleep(1)
+
+print("RUN SEARCHING...")
+sleep(3)
+a, b = MIN, MAX+1
+counter = 0
+while True:
+    center = a + (abs(a-b)//2)
+    sleep(0.1)
+    print(center)
+    if number == center:
+        print("Found in",counter,"iteration(s)")
+        break
+    elif number > center:
+        a = center
+    else:
+        b = center
+    counter += 1
 
