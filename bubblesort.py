f4663948259640b549f5ad76ed1113b32a8e27a5
PetrusAriaa <petrusariacr25@gmail.com>
2023-01-17 03:53:57 +0700

    add descending sort mode

--- bubblesort.py
+++ bubblesort.py
@@ -14,7 +14,20 @@ def bubble_ascending(myArray):
     else:
         return 0
 
+def bubble_descending(myArray):
+    next = 0
+    sleep(0.3)
+    for i in range(0, len(myArray)-1):
+        if myArray[i] < myArray[i+1]:
+            myArray[i+1], myArray[i] = myArray[i], myArray[i+1]
+            next = 1
+    print(myArray)
+    if next == 1:
+        bubble_descending(myArray)
+    else:
+        return 0
+    
 
 bubble_ascending(mySequence)
-
-
+print("=================================")
+bubble_descending(mySequence)
