7d7c5fceb06877faa31cd8a39748059d473326d9
PetrusAriaa <petrusariacr25@gmail.com>
2023-01-19 03:35:40 +0700

    Change loop stop in Integral_trapezoidal()

--- Integral_trapezoidal_rule.py
+++ Integral_trapezoidal_rule.py
@@ -1,38 +1,27 @@
 from math import sin, cos
 
+PROBLEM = '2sin(x)+sin(3x)+2cos(x)+1'
+step_len = 0.5
+start = 0
+stop = 2
+print(f"\nPROBLEM:{PROBLEM}")
+print(f'starting point: {start}')
+print(f'end point: {stop}')
+print(f'length of step: {step_len}')
 
 # default: 6.664165097862194
 def f(x) -> float:
     return 2*sin(x) + sin(3*x) + 2*cos(x) + 1
 
-def Integral_trapezoidal(a, b, step_len) -> float:
+def Integral_trapezoidal(start, stop, step_len) -> float:
+    a = start
+    b = start + step_len
+    result = 0
+    while b <= stop:
+        result += (f(a) + f(b))*step_len/2
+        a = b
+        b += step_len
+    return result
 
-    """
-    Returns the integral of the trapezoidal rule.
-    Parameters
-    ----------
-    a : float
-        lower limit of the integral
-    b : float
-        upper limit of the integral
-    step_len : float
-        length of step
-
-    Returns
-    -------
-    float"""
-    return something
-    
-steps = 0.00001
-start = 0
-stop = 2
-
-a = start
-b = start + step
-result = 0
-
-while a < stop:
-    curr += (f(a) + f(b))*step/2
-    a = b
-    b += step
-
+result = Integral_trapezoidal(start, stop, step_len)
+print(f'\n>> Integral operation result: {result}')
